e91319bac4305c1a63b1a34f9ce3456f57e18410
mohammedtalyb <mohammed.talib@raweng.com>
2019-05-09 15:21:48 +0530

    comments added

--- app.js
+++ app.js
@@ -526,11 +526,12 @@ var jsonData = [
 }
 ]
 
-var data = createId(jsonData);
-
-console.log(data);
-
-function createId(json) {
+var data = editId(jsonData);
+/**
+ * edits the id pf every element
+ * @param {Array} json - original data
+ */
+function editId(json) {
     var data = [];
     var index = 1;
     for(let obj of json) {
@@ -541,148 +542,139 @@ function createId(json) {
     return data;
 }
 
-var table = document.querySelector("table");
-
-//extracting and filling headings
-
 var headings = Object.keys(data[0]);
-var thead = table.createTHead();
-var tr = thead.insertRow();
-for(let heading of headings) {
-    let th = document.createElement("th");
-    let th_capitaliaze = heading.charAt(0).toUpperCase() + heading.slice(1);
-    th.innerText = th_capitaliaze.replace(/_/g," ");
-    tr.appendChild(th);
-}
-var tbody = table.createTBody();
-
+/**
+ * this function invokes the main two function when window loads
+ */
 window.onload = function() {
-loadTable(1);
+    extractingheader(headings);
+    loadTable(1);
 };
 
-
-//filling table data according to page number
+var table = document.querySelector("table");
+/**
+ * this function fills heading section of the table
+ * @param {array} heading - list of headings
+ */
+function extractingheader(headingArr) {
+    var thead = table.createTHead();
+    var tr = thead.insertRow();
+    for(let heading of headingArr) {
+        let th = document.createElement("th");
+        let th_capitaliaze = heading.charAt(0).toUpperCase() + heading.slice(1).replace(/_/g," ");
+        th.innerText = th_capitaliaze;
+        tr.appendChild(th);
+    }
+}
 
 var current_page = 1;
 var records_per_page = 10;
-
-function prevPage()
-{
+/**
+ * this function shows previous page
+ */
+function prevPage(){
     if (current_page > 1) {
         current_page--;
         loadTable(current_page);
     }
 }
-
+/**
+ * this function shows next page
+ */
 function nextPage(){
-if (current_page < numPages()) {
-    current_page++;
-    loadTable(current_page);
-}
+    if (current_page < numPages()) {
+        current_page++;
+        loadTable(current_page);
+    }
 }
 
 var isSearched = false;
-var page_span = document.getElementById("page");
+/**
+ * this function invoke createTableData by deciding table shoulb be created from original data or searched data 
+ * @param {number} page - this is the current page number of table
+ */
 function loadTable(page){
-
-// Validate page
-
-if (page < 1) page = 1;
-if (page > numPages()) page = numPages();
-page_span.innerHTML = page + "/" + numPages();
-if(!isSearched){
-    createTableData(data,page);
-    toggle(page,data);
-} else {
-    createTableData(searchedResults,page);
-    toggle(page,data);
-}
-}
-
-//toggling prev and next button
-function toggle(page,arr) {
-var btn_next = document.getElementById("btn_next");
-var btn_prev = document.getElementById("btn_prev");
-console.log(arr.length);
-if(arr.length<10) {
-    btn_prev.style.visibility = "hidden";
-    btn_next.style.visibility = "hidden";
-}
-if (page == 1) {
-    btn_prev.style.visibility = "hidden";
-} else {
-    btn_prev.style.visibility = "visible";
-}
-
-if (page == numPages()) {
-    btn_next.style.visibility = "hidden";
-} else {
-    btn_next.style.visibility = "visible";
-}
-}
-function numPages(){
-    if(!isSearched) {
-        return Math.ceil(data.length / records_per_page);
+    var page_span = document.getElementById("page");
+    page_span.innerHTML = page + "/" + numPages();
+    if(isSearched){
+        createTableData(searchedResults,page);
+        toggle(page,searchedResults);
     } else {
-        return Math.ceil(searchedResults.length / records_per_page);
+        createTableData(data,page);
+        toggle(page,data);
     }
 }
 
- 
+/**
+ * toggle function decides which button(prev and next) to show or hide
+ * @param {number} page - this is the current page number of table
+ * @param {array} arr - this is the array which can original array or the array created after searching table
+ */
+function toggle(page,arr) {
+    var btnNext = document.getElementById("btnNext");
+    var btnPrev = document.getElementById("btnPrev");
+    if(arr.length<10) {
+        btnPrev.style.visibility = "hidden";
+        btnNext.style.visibility = "hidden";
+    } else if(page == 1) {
+        btnPrev.style.visibility = "hidden";
+        btnNext.style.visibility = "visible";
+    } else if(page == numPages()) {
+        btnNext.style.visibility = "hidden";
+    } else {
+        btnPrev.style.visibility = "visible";
+        btnNext.style.visibility = "visible";
+    }
+}
 
-//searching table contents
-var searchedResults = [];
+/**
+ * creates the total number of pages according to availabe data
+ */
+function numPages(){
+    if(isSearched) {
+        return Math.ceil(searchedResults.length / records_per_page);
+    } else {
+        return Math.ceil(data.length / records_per_page);
+    }
+}
+
+var searchedResults;
+/**
+ * this function checks the input value and search it in data
+ */
 function search() {
     searchedResults = [];
-    var input, filter
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
+    isSearched = false;
+    var input = document.getElementById("myInput");
+    var filter = input.value.toUpperCase();
+    var page_span = document.getElementById("page");
     for(let obj of data) {
-        if(obj.employee_name.toUpperCase().indexOf(filter)!=-1) {
-            searchedResults.push(obj);
-            console.log(searchedResults);
-        }
+        if(obj.employee_name.toUpperCase().indexOf(filter)!=-1) searchedResults.push(obj);
     }
     if(searchedResults) {
         isSearched = true;
+        current_page = 1;
         page_span.innerHTML = current_page + "/" + numPages();
         createTableData(searchedResults,current_page);
-        current_page = 1;
         toggle(current_page,searchedResults);
-        
     }
 }
 
-//creating table
-
-function createTableData(arr,current_page) {
-    // var tr = table.querySelectorAll("tbody tr");
+var tbody = table.createTBody();
+/**
+ * this function creates the table row and fill it with details
+ * @param {Array} tableData - it contains data from which table is to be created
+ * @param {number} current_page - this is the current page number of table
+ */
+function createTableData(tableData,current_page) {
     tbody.innerHTML = "";
     for(let i = (current_page-1)*records_per_page; i<current_page*records_per_page;i++){
         let tr = tbody.insertRow();
-        if(arr[i]) {
+        if(tableData[i]) {
             for(let heading of headings) {
                 let tablecell = tr.insertCell();
-                tablecell.innerText = arr[i][heading];
+                tablecell.innerText = tableData[i][heading];
             }
         }
     }
 }
-
-    // function search() {
-    //     var input, filter, table, tr, td, txtValue;
-    //     input = document.getElementById("myInput");
-    //     filter = input.value.toUpperCase();
-    //     table = document.querySelector("table");
-    //     tr = table.querySelectorAll("tbody tr");
-    //     for (let i = 0; i < tr.length; i++) {    
-    //         td = tr[i].getElementsByTagName("td")[1];
-    //         txtValue = td.textContent || td.innerText;
-    //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    //             tr[i].style.display = "";
-    //         } else {
-    //             tr[i].style.display = "none";
-    //         }     
-    //     }
-    //   }
-
